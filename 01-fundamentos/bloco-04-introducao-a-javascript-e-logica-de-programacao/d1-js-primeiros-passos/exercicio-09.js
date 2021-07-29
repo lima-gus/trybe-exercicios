ab143952dcc7e0fdb357d9f369bfae362d1c9e17
Gustavo Lima <lima_gus@outlook.com>
2021-07-28 23:59:29 -0300

    adiciona exercício 9

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/d1-js-primeiros-passos/exercicio-09.js
@@ -0,0 +1,12 @@
+// Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+
+let a = 1337;
+let b = 15;
+let c = 1998;
+
+if (a % 2 == 1 || b % 2 == 1 || c % 2 == 1) {
+  console.log(true);
+} else {
+  console.log(false);
+}
