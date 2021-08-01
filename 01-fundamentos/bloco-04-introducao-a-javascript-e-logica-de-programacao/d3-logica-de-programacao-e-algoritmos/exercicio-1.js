59d7694de95116c915dc92c3f4307d7ec85b045e
Gustavo Lima <lima_gus@outlook.com>
2021-08-01 19:46:57 -0300

    exercício 1 - bloco 4 - dia 3

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/d3-logica-de-programacao-e-algoritmos/exercicio-1.js
@@ -0,0 +1,12 @@
+let asterisco = '';
+let n = 5;
+
+for (index = 0; index < n; index += 1) {
+  asterisco += '*';
+}
+
+console.log(asterisco);
+
+for (index = 1; index < n; index += 1) {
+  console.log(asterisco); 
+}
