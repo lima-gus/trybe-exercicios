f5411057e5af1975aba17e68e20157b8eae1a804
Gustavo Lima <lima_gus@outlook.com>
2021-08-18 18:45:09 -0300

    Adiciona estados via JavaScript

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/6.1-html-e-css-forms/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/6.1-html-e-css-forms/script.js
@@ -1,3 +1,13 @@
-// arraySiglaEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RO', 'RS', 'RR', 'SC', 'SE', 'SP', 'TO'];
+arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
 
-// arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+const estados = document.getElementById('estado');
+
+function addEstados() {
+  for (i = 0; i < arrayTextoEstado.length; i += 1 ) {
+    const addOption = document.createElement('option');
+    estados.appendChild(addOption).innerText = arrayTextoEstado[i];
+    estados.appendChild(addOption).value = arrayTextoEstado[i];
+  }
+}
+
+addEstados();
