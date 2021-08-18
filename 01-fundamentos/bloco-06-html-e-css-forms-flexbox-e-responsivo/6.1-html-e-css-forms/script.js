70e6c38e04ffdc6716d82cc189db228faf0a0b95
Gustavo Lima <lima_gus@outlook.com>
2021-08-18 16:19:18 -0300

    Parte 1

---
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/6.1-html-e-css-forms/script.js
@@ -0,0 +1,3 @@
+// arraySiglaEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RO', 'RS', 'RR', 'SC', 'SE', 'SP', 'TO'];
+
+// arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
