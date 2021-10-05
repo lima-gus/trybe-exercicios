2c162a813f7edf6d7c31df4ce1071666f8287891
Gustavo Lima <lima_gus@outlook.com>
2021-10-04 22:19:16 -0300

    Exercícios 11.1 - Parte 3 - Content

--- 02-front-end/bloco-11-introducao-a-react/11.1-hello-world-no-react/exercicios-react-pt3/src/App.js
+++ 02-front-end/bloco-11-introducao-a-react/11.1-hello-world-no-react/exercicios-react-pt3/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Content from './Content';
 
 function App() {
   return (
     <div>
       <Header />
+      <Content />
     </div>
   );
 }
