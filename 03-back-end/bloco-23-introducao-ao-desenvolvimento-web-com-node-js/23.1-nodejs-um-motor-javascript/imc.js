4e59fea02eb309418daf2fe8895d003f6bb9fb2e
Gustavo Lima <lima_gus@outlook.com>
2022-02-08 22:03:12 -0300

    feat: exercicios 4 e 5

--- 03-back-end/bloco-23-introducao-ao-desenvolvimento-web-com-node-js/23.1-nodejs-um-motor-javascript/imc.js
+++ 03-back-end/bloco-23-introducao-ao-desenvolvimento-web-com-node-js/23.1-nodejs-um-motor-javascript/imc.js
@@ -10,6 +10,30 @@ const imc = (peso, altura) => {
   const calcImc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
 
   console.log(`Peso: ${peso}, Altura: ${altura}, IMC: ${calcImc}`);
+
+  if (calcImc < 18.5) {
+    return console.log('Abaixo do peso (magreza)');
+  } 
+  
+  if (calcImc > 18.5 && calcImc < 24.9) {
+   return  console.log('Peso normal');
+  }
+
+  if (calcImc > 25.0 && calcImc < 29.9) {
+    return console.log('Acima do peso (sobrepeso)');
+  }
+
+  if (calcImc > 30.0 && calcImc < 34.9) {
+    return console.log('Obesidade grau I');
+  }
+
+  if (calcImc > 35.0 && calcImc < 39.9) {
+    return console.log('Obesidade grau II');
+  }
+
+  if (calcImc > 40.0) {
+    return console.log('Obesidade graus III e IV');
+  }
 }
 
 imc();
