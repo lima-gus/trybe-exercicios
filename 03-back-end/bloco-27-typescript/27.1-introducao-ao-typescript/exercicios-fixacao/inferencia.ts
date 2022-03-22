232e9e7ae0db0ce4cb8f8aa6ff3f45d8d1e831c7
Gustavo Lima <lima_gus@outlook.com>
2022-03-22 12:11:39 -0300

    feat: 27.1 - typescript: tipos e subtipos

---
+++ 03-back-end/bloco-27-typescript/27.1-introducao-ao-typescript/exercicios-fixacao/inferencia.ts
@@ -0,0 +1,3 @@
+let flag = true; // o compilador vai inferir o tipo boolean
+let numberPI = 3.1416; // o compilador vai inferir o tipo number
+let message = 'Hello World!'; // o compilador vai inferir o tipo string
---
+++ 03-back-end/bloco-27-typescript/27.1-introducao-ao-typescript/exercicios-fixacao/number.ts
@@ -0,0 +1,4 @@
+let x: number;
+
+let y: number = 0;
+let z: number = 123.456;
